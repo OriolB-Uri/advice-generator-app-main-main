9aedb3da14a6718dddb35b0c9b74ab1e98d74fb7
OriolB-Uri <oriolbotella@hotmail.com>
2023-10-31 10:49:38 +0100

    hecho

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// ¡Buena suerte!
+const button = document.querySelector("#get-advice-button");
+
+async function updateNewAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice"); 
+    const randomAdvice = await response.json();
+  
+    console.log("Nuevo consejo!", randomAdvice);
+    document.querySelector("#advice-id").textContent = randomAdvice.slip.id;
+    document.querySelector(".card__quote").textContent = randomAdvice.slip.advice;  }
+  
+  button.addEventListener("click", updateNewAdvice);
